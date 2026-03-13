adb14b4e24eee3cd805211b255184fa1b74c3bae
Evayuan824 <yuan.ev+1@northeastern.edu>
2026-03-13 13:05:43 -0400

    Update sketch.js

--- assignment-5/sketch.js
+++ assignment-5/sketch.js
@@ -1,17 +1,71 @@
+/* 
+Prototyping with Code
+Eva Yuan | yuan.ev@northeastern.edu
+Assignment 5 | Ocean Waves
+*/ 
+
+// global variables 
+let t = 0; // time 
+let layers = 10; // number of waves 
+
+let amplitude = [];
+let frequency = [];
+let speed = [];
+let phase = [];
+let baseY = [];
+let waveColor = [];
+
+// runs on any sized canvas 
 function setup() {
-  // Create a canvas as big as the container window
   createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER, CENTER);
+  noStroke();
+  
+  let waveAmplitude = height * 0.05;
+  let waveFrequency = 0.01;
+  
+  for (let i = 0; i < layers; i++) {
+    amplitude[i] = waveAmplitude;
+    frequency[i] = waveFrequency;
+    speed[i] = random(0.01, 0.02);
+    phase[i] = random(TWO_PI);
+
+    baseY[i] = map(i, 0, layers - 1, height * 0.2, height * 0.85);
+
+    // gradient waves 
+    waveColor[i] = lerpColor(
+      color(20, 70, 140), // dark blue
+      color(150, 200, 240), // light blue
+      i / (layers - 1)
+    );
+  }
 }
 
+// creating the waves 
 function draw() {
-  background(220);
-  textSize(height / 16);
-  text("Assignment[5]", width / 2, height / 2);
+  background(200, 220, 240);
+  
+  for (let i = 0; i < layers; i++) {
+
+    fill(waveColor[i]);
+
+    beginShape();
+    vertex(0, height);
+
+    for (let x = 0; x <= width; x += 10) {
+      let waveHeight =
+        sin(x * frequency[i] + t * speed[i] + phase[i]) * amplitude[i];
+      let y = baseY[i] + waveHeight;
+      vertex(x, y);
+    }
+    vertex(width, height);
+    endShape(CLOSE);
+  }
+
+// speed of waves 
+  t += 1;
 }
 
-// This function is called any time the window is resized
+// resizes with browser 
 function windowResized() {
-  // Change the canvas size to fit the window
   resizeCanvas(windowWidth, windowHeight);
 }

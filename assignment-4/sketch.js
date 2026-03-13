5e54992f3378fdcb79c3776dd055b48d408107c1
Evayuan824 <yuan.ev+1@northeastern.edu>
2026-03-13 13:03:04 -0400

    Update sketch.js

--- assignment-4/sketch.js
+++ assignment-4/sketch.js
@@ -1,17 +1,65 @@
+/* 
+Prototyping with Code
+Eva Yuan | yuan.ev@northeastern.edu
+Assignment 4 | Valentine's Day Wrapping Paper
+*/ 
+
 function setup() {
-  // Create a canvas as big as the container window
-  createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER, CENTER);
+  createCanvas(1024, 1024);
+  noLoop();
 }
 
 function draw() {
-  background(220);
-  textSize(height / 16);
-  text("Assignment[4]", width / 2, height / 2);
+  Background();
+  Pattern();
+}
+  
+// gradient background
+function Background() {
+  noStroke();
+
+  for (let y = 0; y < height; y++) {
+    let r = map(y, 0, height, 255, 255);
+    let g = map(y, 0, height, 180, 80);
+    let b = map(y, 0, height, 220, 120);
+    fill(r, g, b);
+    rect(0, y, width, 1);
+  }
+}
+  
+function Pattern() {
+  // spacing of shapes
+  let spacing = 51;
+  let size = 25;
+
+  for (let x = spacing / 2; x < width; x += spacing) {
+    for (let y = spacing / 2; y < height; y += spacing) {
+
+      // color selector
+      if (random() < 0.5) {
+        fill(255, 255, 255, 180); // white
+      } else {
+        fill(255, 52, 116, 180); // pink
+      }
+      noStroke();
+
+      // circle & diamond placement
+      if (Math.floor(x / spacing) % 2 === 0) {
+        circle(x, y, size);
+      } else {
+        push();
+        translate(x, y);
+        rotate(PI / 4);
+        rectMode(CENTER);
+        square(0, 0, size);
+        pop();
+      }
+    }
+  }
 }
 
-// This function is called any time the window is resized
-function windowResized() {
-  // Change the canvas size to fit the window
-  resizeCanvas(windowWidth, windowHeight);
+function keyPressed() {
+  if (key === 'S' || key === 's') {
+    saveCanvas('assignment4_pattern_yuan_eva', 'png');
+  }
 }
